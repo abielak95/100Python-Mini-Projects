3eeeebb4d149ff2f37b16679aa51d525a6444aa8
agnieszka95 <agnieszka.bielak@hotmail.com>
2018-01-31 00:42:14 +0100

    Task zero

---
+++ 0. NameGenerator.py
@@ -0,0 +1,8 @@
+#Name generator
+
+import random
+
+names = ['Ann', 'Robert', 'Elsa', 'Filippe']
+surnames = ['John', 'Smith', 'Kowalski']
+
+print(random.choice(names) + ' ' + random.choice(surnames))
